f2cd330afec0fdf0bc792b6a76dec19c38ac7b99
Juan Manuel Cataldo Pavan <juanocataldo@MacBook-Air-de-Juan.local>
2024-02-13 14:27:33 -0300

    test

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('¡Hola desde Express!');
+app.get('/qring', (req, res) => {
+  res.send('¡Te estan tocando el timbre!');
 });
 
 app.listen(PORT, () => {
